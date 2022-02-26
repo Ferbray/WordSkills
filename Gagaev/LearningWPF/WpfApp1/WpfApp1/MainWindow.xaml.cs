d3cbd64b95bf6805abe3f918680041b080d42aad
GIS <yt_ferbray@mail.ru>
2022-02-26 10:46:47 +0300

    Some Changes 1.0.8

---
+++ Gagaev/LearningWPF/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Button());
+            Manager.MainFrame = MainFrame;
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs a)
+        {
+            Manager.MainFrame.GoBack();
+        }
+    }
+}
