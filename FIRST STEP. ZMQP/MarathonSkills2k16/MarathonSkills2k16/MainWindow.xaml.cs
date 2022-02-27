12aa7cc7c2e1267ce0abc420b45c7562cdd3a5f9
zmqp <domarlaid2@mail.ru>
2022-02-27 17:06:26 +0300

    My app 1.1.0

---
+++ FIRST STEP. ZMQP/MarathonSkills2k16/MarathonSkills2k16/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MarathonSkills2k16
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_Initialized(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            string whatsDay = dt.ToString("dddd d MMMM yyyy");
+            (sender as TextBlock).Text = $"{whatsDay}";
+        }
+
+    }
+}
