dc14b2311bc9c89b3d4ef1eaaa8f30505f227e80
GIS <yt_ferbray@mail.ru>
2022-02-27 18:05:33 +0300

    New changes 1.0.9

---
+++ Gagaev/MarathonWPF/MarathonWPF/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_Initialized(object sender, EventArgs e)
+        {
+            DateTime time = DateTime.Now;
+            (sender as TextBlock).Text = "Москва, Россия\n" + time.ToString("dddd d MMMM yyyy");
+            
+        }
+    }
+}
